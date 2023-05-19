dc2c411dc532766820916fc8ee4e3d80c277758a
FORNO <forno@xmaho.link>
2023-05-19 20:08:14 +0900

    Fix struct document
    Signed-off-by: FORNO <forno@xmaho.link>

--- src/struct.ts
+++ src/struct.ts
@@ -10,8 +10,8 @@
  * @since 0.0.1
  * @category struct
  * @example
- *   import { lookup } from '@xmaho&#x2F;utility-ts'
- *   assert.deepStrictEqual(lookup('a')({a: 'hello'}), 'hello')
+ *   import struct from '@xmaho&#x2F;utility-ts'
+ *   assert.deepStrictEqual(struct.lookup('a')({a: 'hello'}), 'hello')
  */
 export const lookup =
   <K extends string | number | symbol>(path: K) =>
