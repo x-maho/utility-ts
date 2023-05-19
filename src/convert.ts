95508af711d7b3635141574133d5ac711ca5742f
FORNO <forno@xmaho.link>
2023-05-19 20:57:04 +0900

    Fix module resolve
    Signed-off-by: FORNO <forno@xmaho.link>

--- src/convert.ts
+++ src/convert.ts
@@ -2,7 +2,7 @@
 
 import * as O from 'fp-ts/Option'
 import { Option } from 'fp-ts/Option'
-import { pipe } from 'fp-ts/lib/function'
+import { pipe } from 'fp-ts/function'
 
 // -----------------------------------------------------------------------------
 // convert
