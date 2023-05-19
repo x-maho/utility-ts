5b11e7ca275b03f4cfd9d01f086682ecf76285ca
FORNO <forno@xmaho.link>
2023-05-19 20:37:21 +0900

    Update taskify to liftPromise
    Signed-off-by: FORNO <forno@xmaho.link>

--- src/TaskEither.ts
+++ src/TaskEither.ts
@@ -7,15 +7,15 @@ import * as TE from 'fp-ts/TaskEither'
 // -----------------------------------------------------------------------------
 
 /**
- *
+ * It lifts a function that returns a Promise to a function that returns a TaskEither
  *
  * @since 0.0.3
  * @category TaskEither
  * @example
  *   import { TaskEither } from '@xmaho/utility-ts'
- *   const task = TaskEither.taskify(E.toError)((a: string) => Promise.resolve(a)) // the task is a TaskEither<Error, string>
+ *   const task = TaskEither.liftPromise(E.toError)((a: string) => Promise.resolve(a)) // the task is a TaskEither<Error, string>
  */
-export const taskify =
+export const liftPromise =
   <E>(onRejected: (e: unknown) => E) =>
   <A, Args extends unknown[]>(f: (...args: Args) => Promise<A>) =>
   (...args: Args) => {
